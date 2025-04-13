c5ae2fbfcfff1d0a6e405670a95a5e89f949cc74
gabrielmattanosilva <gabrielmattanosilva@gmail.com>
2025-04-13 20:17:25 -0300

    RP2040 internal temperature

--- projetos/temp_interna_RP2040/README.md
+++ projetos/temp_interna_RP2040/README.md
@@ -1,6 +1,9 @@
-# Unidade 1 - Tarefa: Leitura dos sinais analógicos do joystick
+# Unidade 1 - Tarefa: Monitor da temperatura interna da MCU
 
 ## Enunciado:
 Faça um programa em C para ler a temperatura interna do RP2040. Converta a leitura do ADC em um valor em °C.
 
 ---
+
+Baseado no datashhet do RP2040 (página 563 - 4.9.5.Temperature Sensor)
+https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf
---
+++ projetos/temp_interna_RP2040/src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+
+#define ADC_TEMPERATURE_CHANNEL 4
+
+// Função que converte o valor lido do ADC (valor digital de 12 bits) para a temperatura em graus Celsius
+float adc_to_temperature(uint16_t adc_value)
+{
+    const float conversion_factor = 3.3f / (1 << 12);               // Fator de conversão de 12 bits (0-4095) para o intervalo de 0-3.3V
+    float adc_voltage = adc_value * conversion_factor;              // Converte o valor do ADC para tensão
+    float temperature = 27.0f - (adc_voltage - 0.706f) / 0.001721f; // Fórmula fornecida no datasheet para conversão em Celsius
+    return temperature;
+}
+
+int main()
+{
+    stdio_init_all();
+    adc_init();
+    adc_set_temp_sensor_enabled(true);
+    adc_select_input(ADC_TEMPERATURE_CHANNEL);
+
+    while (true)
+    {
+        uint16_t adc_value = adc_read();
+        float temperature = adc_to_temperature(adc_value);
+        printf("Temperatura: %.2f °C\n", temperature);
+        sleep_ms(1000);
+    }
+
+    return 0;
+}
