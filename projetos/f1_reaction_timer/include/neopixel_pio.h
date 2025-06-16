812eea66f82e0602153184f1d4f93aa2c3a9ecc3
Gabriel <gabrielmattanosilva@gmail.com>
2025-06-16 17:59:42 -0300

    neopixel lib added

---
+++ projetos/f1_reaction_timer/include/neopixel_pio.h
@@ -0,0 +1,11 @@
+#ifndef NEOPIXEL_PIO_H
+#define NEOPIXEL_PIO_H
+
+// Definição do número de LEDs e pino.
+#define LED_COUNT 25
+#define LED_PIN 7
+
+void npSetF1Lights();
+void npClear();
+
+#endif // NEOPIXEL_PIO_H
